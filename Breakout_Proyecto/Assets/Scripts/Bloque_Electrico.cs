4a1432c239895b28b4992fd4fa540fb3ca1bb963
Paolo Mar <PaoloMar_@hotmail.com>
2021-12-29 09:12:04 -0600

    Tarea Bloques de Hielo y Eléctrico

---
+++ Breakout_Proyecto/Assets/Scripts/Bloque_Electrico.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bloque_Electrico : Bloque
+{
+    //Este Cubo al ser golpeado hará que la pelota se acelere
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        resistencia = 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Breakout_Proyecto/Assets/Scripts/Bloque_Hielo.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bloque_Hielo : Bloque
+{
+    //Este Cubo al ser golpeado ralentizará a la pelota
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        resistencia = 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
