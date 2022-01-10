076932a7ecabf4ea02bbfb58cebf10aef388dd85
Paolo Mar <PaoloMar_@hotmail.com>
2022-01-10 12:46:04 -0600

    modulo 12

--- Breakout_Proyecto/Assets/Scripts/Bola.cs
+++ Breakout_Proyecto/Assets/Scripts/Bola.cs
@@ -7,7 +7,10 @@ public class Bola : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Vector3 posicionInicial = GameObject.FindGameObjectWithTag("Jugador").transform.position;
+        posicionInicial.y += 3;
+        this.transform.position = posicionInicial;
+        this.transform.SetParent(GameObject.FindGameObjectWithTag("Jugador").transform);
     }
 
     // Update is called once per frame
