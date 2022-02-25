70859f3880e84bae5427c344bfdc9d1ebf76c745
Paolo Mar <PaoloMar_@hotmail.com>
2022-02-25 11:07:24 -0600

    a

--- Breakout_Proyecto/Assets/Scriptable Objects/Opciones.cs
+++ Breakout_Proyecto/Assets/Scriptable Objects/Opciones.cs
@@ -2,13 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName ="Opciones", menuName ="Herramientas/Opciones" , order = 1)]
+[CreateAssetMenu(fileName ="Opciones", menuName ="Herramientas/Opciones", order = 1)]
 
 public class Opciones : PuntajePersistente
 {
     public float velocidadBola = 30;
     public dificultad NivelDificultad = dificultad.facil;
-    // Start is called before the first frame update
+
 public enum dificultad
     {
         facil,
